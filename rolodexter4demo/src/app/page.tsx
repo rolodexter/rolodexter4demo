51654a43d847643914481608e18ac0580365e0d6
rolodexter <jmaristela2023@gmail.com>
2025-03-03 11:27:13 +0800

    Update page.tsx

--- rolodexter4demo/src/app/page.tsx
+++ rolodexter4demo/src/app/page.tsx
@@ -1,1 +1,13 @@
- 
+import { DataStreamSimulator } from '@/components/simulation/DataStreamSimulator';
+import { CursorSimulation } from '@/components/simulation/CursorSimulation';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <div className="relative w-full h-screen">
+        <DataStreamSimulator />
+        <CursorSimulation />
+      </div>
+    </main>
+  );
+} 
